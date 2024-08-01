c5546153f7748c235447c35fa39e124755884406
UzairKhan-27 <m.uzairkhan1000@gmail.com>
2024-08-01 19:45:20 +0500

    Add about us content

--- src/about.js
+++ src/about.js
@@ -1,9 +1,14 @@
 function aboutUsPage()
 {
     const content=document.querySelector("#content");
+
+
     content.textContent="";
-    const heading=document.createElement("h1");
-    heading.textContent="Welcome To About Foodies Club";
-    content.appendChild(heading);
+    const aboutUs=document.createElement("div");
+    aboutUs.classList.add("about-us-text");
+    aboutUs.textContent="Foodies Club was founded in 2005 by a group of culinary enthusiasts who wanted to create a dining experience like no other. Our mission has always been to bring people together through a shared love of food and culture."
+    content.appendChild(aboutUs);
+    aboutUs.textContent="Foodies Club was founded in 2005 by a group of culinary enthusiasts who wanted to create a dining experience like no other. Our mission has always been to bring people together through a shared love of food and culture."
+    content.appendChild(aboutUs);
 }
 export  {aboutUsPage}
