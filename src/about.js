e1cb479494dfca421aace7af18a1a6d577aa8687
UzairKhan-27 <m.uzairkhan1000@gmail.com>
2024-08-01 22:05:50 +0500

    Add content on about us page

--- src/about.js
+++ src/about.js
@@ -8,9 +8,22 @@ function aboutUsPage()
     aboutUs.classList.add("about-us-text");
     content.appendChild(aboutUs);
 
-    const aboutUsItem=document.createElement("div");
-    aboutUsItem.classList.add("about-us-item");
-    aboutUsItem.textContent="Foodies Club was founded in 2005 by a group of culinary enthusiasts who wanted to create a dining experience like no other. Our mission has always been to bring people together through a shared love of food and culture."
-    aboutUs.appendChild(aboutUsItem);
+    const aboutUsItemOne=document.createElement("div");
+    aboutUsItemOne.classList.add("about-us-item");
+    aboutUsItemOne.textContent="Foodies Club was founded in 2005 by a group of culinary enthusiasts who wanted to create a dining experience like no other. Our mission has always been to bring people together through a shared love of food and culture."
+    aboutUs.appendChild(aboutUsItemOne);
+
+    const aboutUsItemTwo=document.createElement("div");
+    aboutUsItemTwo.classList.add("about-us-item");
+    aboutUsItemTwo.textContent="At Foodies Club, we believe that food is more than just sustenance. It's an experience that should be savored and shared. We are committed to using the freshest ingredients and time-honored cooking techniques to create dishes that are both innovative and comforting."
+    aboutUs.appendChild(aboutUsItemTwo);
+
+
+    const aboutUsItemThree=document.createElement("div");
+    aboutUsItemThree.classList.add("about-us-item");
+    aboutUsItemThree.textContent= "Whether you're here for a special occasion or just a casual meal, we invite you to join us at Foodies Club. Come experience our passion for food, our commitment to quality, and our love for bringing people together."
+    aboutUs.appendChild(aboutUsItemThree);
+
+
 }
 export  {aboutUsPage}
