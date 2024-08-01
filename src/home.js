38c3617350121c43a2906bd1a1347e3313ce7b15
UzairKhan-27 <m.uzairkhan1000@gmail.com>
2024-08-01 18:14:48 +0500

    Add class to homepage heading

--- src/home.js
+++ src/home.js
@@ -1,9 +1,11 @@
 function homePage()
 {
     const content=document.querySelector("#content");
+    content.textContent="";
     const heading=document.createElement("h1");
     heading.textContent="Welcome To Foodies Club";
+    heading.classList.add("home-heading");
+    content.style.display="flex";
     content.appendChild(heading);
-    console.log("HERE");
 }
 export {homePage};
