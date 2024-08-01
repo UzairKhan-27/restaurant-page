f85c78cdedd95c55e50bf7db607295ad3a5264d6
UzairKhan-27 <m.uzairkhan1000@gmail.com>
2024-08-01 15:52:08 +0500

    Create about page

---
+++ src/about.js
@@ -0,0 +1,9 @@
+function aboutUsPage()
+{
+    const content=document.querySelector("#content");
+    const heading=document.createElement("h1");
+    heading.textContent="Welcome To About Foodies Club";
+    content.appendChild(heading);
+    console.log("HERE");
+}
+export  {aboutUsPage}
