6a01dd61baf0345f79e45fa92a0e1dd5317434e6
UzairKhan-27 <m.uzairkhan1000@gmail.com>
2024-08-01 21:40:45 +0500

    Add grid container and items

--- src/about.js
+++ src/about.js
@@ -6,9 +6,11 @@ function aboutUsPage()
     content.textContent="";
     const aboutUs=document.createElement("div");
     aboutUs.classList.add("about-us-text");
-    aboutUs.textContent="Foodies Club was founded in 2005 by a group of culinary enthusiasts who wanted to create a dining experience like no other. Our mission has always been to bring people together through a shared love of food and culture."
     content.appendChild(aboutUs);
-    aboutUs.textContent="Foodies Club was founded in 2005 by a group of culinary enthusiasts who wanted to create a dining experience like no other. Our mission has always been to bring people together through a shared love of food and culture."
-    content.appendChild(aboutUs);
+
+    const aboutUsItem=document.createElement("div");
+    aboutUsItem.classList.add("about-us-item");
+    aboutUsItem.textContent="Foodies Club was founded in 2005 by a group of culinary enthusiasts who wanted to create a dining experience like no other. Our mission has always been to bring people together through a shared love of food and culture."
+    aboutUs.appendChild(aboutUsItem);
 }
 export  {aboutUsPage}
