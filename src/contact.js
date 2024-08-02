cb97e89f4b34c8b38d442227e071d0bb6e412a72
UzairKhan-27 <m.uzairkhan1000@gmail.com>
2024-08-02 15:46:58 +0500

    Add content on contact us page

--- src/contact.js
+++ src/contact.js
@@ -2,8 +2,24 @@ function contactUsPage()
 {
     const content=document.querySelector("#content");
     content.textContent="";
-    const heading=document.createElement("h1");
-    heading.textContent="Contact Us";
-    content.appendChild(heading);
+    const contactUs=document.createElement("div");
+    contactUs.classList.add("contact-us");
+    content.appendChild(contactUs);
+
+    const address=document.createElement("div");
+    address.classList.add("address");
+    address.textContent="Visit Our Branch at Random Location Anywhere";
+
+    const email=document.createElement("div");
+    email.classList.add("email");
+    email.textContent="Email Us at foodies@gmail.com";
+
+    const number=document.createElement("div");
+    number.classList.add("number");
+    number.textContent="Call us at 0300-0101010";
+
+    contactUs.appendChild(address);
+    contactUs.appendChild(email);
+    contactUs.appendChild(number);
 }
 export  {contactUsPage}
